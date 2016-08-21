f847c2eca88643007b10694559d1cce7cf1de177
Scott O'Malley <scottyomalley@gmail.com>
2016-08-21 20:25:57 +0100

    Update iff_membership_lookup.php
    Version Bump

--- iff_membership_lookup.php
+++ iff_membership_lookup.php
@@ -3,7 +3,7 @@
 Plugin Name: IFF Membership Lookup
 Description: Plugin for displaying the current licecend members of the IFF
 Author: Scott O'Malley
-Version: 1.4
+Version: 1.5
 GitHub Plugin URI: https://github.com/thehandsomecoder/IFFMembershipLookupTool
 GitHub Branch:     master
 */
